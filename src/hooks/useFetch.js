0f7172b9d0d1b7349e7c6ccc68f760783db48b5e
Rati <ratigumashvili@gmail.com>
2022-11-10 16:34:44 +0400

    added hooks

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import {
+  setDataWithExpiry,
+  getDataWithExpiry,
+} from "../helpers/getLocalStorage";
+
+const URL = `http://my-json-server.typicode.com/DanielBarbakadze/Advanced-JS-and-React-Basics/db`;
+
+const useFetch = () => {
+  const [fetchedData, setFetchData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(URL);
+      const data = await response.json();
+      setDataWithExpiry("data", data, 600000);
+      setFetchData(data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("data")) {
+      setFetchData(getDataWithExpiry("data"));
+    } else {
+      fetchData();
+    }
+  }, []);
+
+  return { fetchedData, loading };
+};
+
+export default useFetch;
